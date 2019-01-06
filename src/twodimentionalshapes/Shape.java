2102b2689f083f2c883bc6cfc83a0ca1b3f12ece
breadboykid <43177212+breadboykid@users.noreply.github.com>
2019-01-06 02:58:23 +0000

    Update Shape.java

--- src/twodimentionalshapes/Shape.java
+++ src/twodimentionalshapes/Shape.java
@@ -7,24 +7,24 @@ public abstract class Shape{
     Scanner scan = new Scanner(System.in);
 
     String shapeName;
+    public static int SIDES, SIDES_REQUIRED;
 
-    Shape(String name){
+    Shape(String name, int newSide, int newRequiredLengths){
         this.shapeName = name;
+        this.SIDES = newSide;
+        this.SIDES_REQUIRED = newRequiredLengths;
     }
 
-    public void getLengths(){
+    /*public void getLengths(){
         double[] iter = new double[getNumberOfSidesNeeded()];
 
         for(int i = 0; i < getNumberOfSides(); i++){
             System.out.println("Enter side" + (i+1) + ":");
             iter[i] = scan.nextDouble();
         }
-    }
-
-
+    }*/
 
     abstract public double getArea();
     abstract public double getPerimeter();
-    abstract public int getNumberOfSides();
-    abstract public int getNumberOfSidesNeeded();
+    abstract public void setLengths();
 }
